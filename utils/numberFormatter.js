113636fc9ea1adafb7db0c8f7471093e4d2f1401
Samson <sams1307wolde@gmail.com>
2026-03-08 11:07:09 +0300

    Commit updates to formatter

--- utils/numberFormatter.js
+++ utils/numberFormatter.js
@@ -2,13 +2,13 @@ function formatPhoneNumber(phoneNumber) {
   // Remove all non-digit characters
   let cleaned = phoneNumber.replace(/\D/g, "");
 
-  // Add country code if missing (assuming +91 for India)
+  // Add country code if missing (assuming +251 for Ethiopia)
   if (!cleaned.startsWith("+")) {
     // Check if it starts with 0
     if (cleaned.startsWith("0")) {
-      cleaned = "91" + cleaned.substring(1);
+      cleaned = "251" + cleaned.substring(1);
     } else if (cleaned.length === 10) {
-      cleaned = "91" + cleaned;
+      cleaned = "251" + cleaned;
     }
     cleaned = "+" + cleaned;
   }
