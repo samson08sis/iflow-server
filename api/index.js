aec52d757dbfa4fc5877d5f86460925f9a020c46
Samson <sams1307wolde@gmail.com>
2026-03-08 11:04:56 +0300

    prepare server for Vercel

--- api/index.js
+++ api/index.js
@@ -2,14 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const authRoutes = require("./routes/authRoutes");
+const authRoutes = require("../routes/authRoutes");
 
 const app = express();
 
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:8081", "exp://localhost:8081"],
+    origin: [
+      "http://localhost:8081",
+      "http://localhost:8082",
+      "exp://localhost:8081",
+      "exp://localhost:8082",
+    ],
     credentials: true,
   })
 );
@@ -23,6 +28,9 @@ mongoose
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/test", (req, res) => {
+  return res.json({ hi: "Helli thereafjahd!" });
+});
 
 // Error handling middleware
 app.use((err, req, res, next) => {
@@ -30,7 +38,10 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Something went wrong!" });
 });
 
-const PORT = process.env.PORT || 7002;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 7002;
+// app.listen(PORT, "0.0.0.0", () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+// Export the app for Vercel
+module.exports = app;
